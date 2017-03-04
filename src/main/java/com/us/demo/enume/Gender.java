6bcfad4f4f969a3aa6d5cd7d2b78c5923c550fd7
Sean Yan <huaianyanxu@163.com>
2017-03-05 01:53:17 +0800

    Enume Test.

--- src/main/java/com/us/demo/enume/Gender.java
+++ src/main/java/com/us/demo/enume/Gender.java
@@ -1,6 +1,14 @@
 package com.us.demo.enume;
 
 public enum Gender {
-	
-
+	EXCEPTION("异常");
+	private String name;
+	Gender(){
+		name="";
+	}
+	Gender(String name){this.name=name;}
+	public String getName(){
+		return name;
+	}
 }
+
---
+++ src/main/java/com/us/demo/enume/GenderTest.java
@@ -0,0 +1,9 @@
+package com.us.demo.enume;
+
+public class GenderTest {
+	public static void main(String []args){
+		// 这样使用Enume 的方法 方便更新和处理 需要使用的常量字符串;方便维护和更新;
+		System.out.println(Gender.EXCEPTION.getName());
+	}
+
+}
